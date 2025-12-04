88f802058825e1b4ce2108d6b60f8f2bd9bb3a15
[a]ster <personal.asterious@gmail.com>
2025-12-05 01:08:55 +0300

    develop | base password class

--- SafePass_local-password-manager/Program.cs
+++ SafePass_local-password-manager/Program.cs
@@ -9,5 +9,17 @@ namespace SafePass_local_password_manager
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
+
+        public class PasswordEntry
+        {
+            public int id {  get; set; }
+            public string? service { get; set; }
+            public string? username { get; set; } 
+            public string? encryptedPassword { get; set; } 
+            public DateTime created { get; set; }
+            public DateTime updated { get; set; }
+        }
+
+        
     }
 }
